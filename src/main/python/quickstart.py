8d10f27a5f2d07b669dce4679bb89ad93abd83e4
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-08-25 22:52:32 +0530

    added functions

--- src/main/python/quickstart.py
+++ src/main/python/quickstart.py
@@ -20,7 +20,7 @@ CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 
-def get_credentials():
+def get_credentials( credentailFile ):
     """Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
@@ -34,7 +34,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+                                   credentailFile)
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -48,34 +48,62 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+def headers(service):
+    """
+    Fetch "from" and "subject"
+    """
+    print ("Calling Headers")
+
+def bodySnippet(service):
+    "fetch body snippet"
+    print ( "calling body snippet")
+    results = service.users().messages().list(userId='me').execute()
+
+    # labels = service.users().messages().get(userId="me", id="15e19a83eaed02de").execute()
+
+    # print('Message snippet: %s' % message['snippet'])
+
+
+    # for label in results:
+    #     print(results[label])
+    # for arrayVal in results
+    # message []
+
+    # Get functions for
+    #       From, Subject, Body
+
+    # Modify credential path to be able to take multiple credentials.
+    i = 0
+
+    for dictValues in results.get('messages'):
+        i += 1
+        messageText = service.users().messages().get(userId="me", id=dictValues.get('id')).execute()
+        # print(messageText['snippet'])
+        print(messageText['payload']['headers'])
+        print("---------------------------------------")
+        if i > 5:
+            break
+
+
 def main():
     """Shows basic usage of the Gmail API.
 
     Creates a Gmail API service object and outputs a list of label names
     of the user's Gmail account.
     """
-    credentials = get_credentials()
+
+    """ Create a credential file which will have username binded to it dynamically 
+    """
+    mainCredentialFile =  "gmail-python-quickstart-vineet.json"
+    credentials = get_credentials( mainCredentialFile )
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    results = service.users().messages().list(userId='me').execute()
+    # Fetch "From" and "Subject"
+    headers(service)
+    # Fetch Body Snippet
+    bodySnippet(service)
 
-
-    #labels = service.users().messages().get(userId="me", id="15e19a83eaed02de").execute()
-
-    #print('Message snippet: %s' % message['snippet'])
-
-
-    # for label in results:
-    #     print(results[label])
-    # for arrayVal in results
-    #message []
-
-    for dictValues in results.get('messages'):
-        messageText = service.users().messages().get(userId="me", id= dictValues.get('id') ).execute() 
-        #print(messageText['snippet'])
-        print(messageText['payload']['headers'])
-        print("---------------------------------------")
 
 if __name__ == '__main__':
     main()
